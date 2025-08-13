0a1f297e6779f84f86c75d2b591a0fd74459a0f6
CarolineOliveiraSilva <carolline1995@hotmail.com>
2025-08-13 10:38:09 -0300

    aula sobre variável composta ,vetores

---
+++ aula15/vetor.js
@@ -0,0 +1,10 @@
+let num =[0,1,7,4,5]
+num.sort()
+/*
+for (let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]} `)
+}*/
+
+for(let pos in num) {
+    console.log(`A  posição ${pos} tem o valor ${num[pos]}`)
+}
